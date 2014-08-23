ec4f6c784c37f56df46dd7f6c25fa021c8696059
ozr <ozr@ricedonate.com>
2014-08-23 16:29:14 +0800

    new

---
+++ Calculator/Calculator/CalculatorView.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorView.h
+//  Calculator
+//
+//  Created by megil on 8/23/14.
+//  Copyright (c) 2014 megil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorView : UICollectionView
+
+@end
---
+++ Calculator/Calculator/CalculatorView.m
@@ -0,0 +1,31 @@
+//
+//  CalculatorView.m
+//  Calculator
+//
+//  Created by megil on 8/23/14.
+//  Copyright (c) 2014 megil. All rights reserved.
+//
+
+#import "CalculatorView.h"
+
+@implementation CalculatorView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
